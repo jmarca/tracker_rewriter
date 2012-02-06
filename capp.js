e25bca35ca66eb5564dc3775a681433a79418fe0
James E. Marca <jmarca@translab.its.uci.edu>
2012-02-06 11:41:09 -0800

    tweaked localhost to 127.0.0.1, and moved service to lib/rewrite.js modified:   capp.js new file:   lib/rewrite.js modified:   package.json

--- capp.js
+++ capp.js
@@ -2,26 +2,23 @@ var connect = require('connect');
 var request = require('request');
 var path = require('path');
 var filed = require('filed');
+var rewriter = require('./lib/rewrite.js').rewriter;
 
-
-//var RedisStore = require('connect-redis')(connect);
+var RedisStore = require('connect-redis')(connect);
 
 var server = connect.createServer(
     connect.logger()
-//    ,connect.favicon(__dirname + '/public/favicon.ico')
-//    ,connect.bodyParser()
-//    ,connect.cookieParser()
-//    ,connect.session({ store: new RedisStore   //RedisStore or MemoryStore
-//                       , secret: '2pz.sdfv354t' })
-    ,connect.router(user)
+    ,connect.favicon(__dirname + '/public/favicon.ico')
+    ,connect.bodyParser()
+    ,connect.cookieParser()
+    ,connect.session({ store: new RedisStore   //RedisStore or MemoryStore
+                       , secret: '2pz.sdfv354t' })
+    ,connect.router(vds)
+    ,connect.router(wim)
     ,connect.router(files)
     ,connect.errorHandler({ dumpExceptions: true, showStack: true })
 );
 
-var _ = require('underscore');
-
-
-var couch = "http://localhost:5984";
 // var rewrites = [
 //     {from:'/', to:'index.html'}
 //                , {from:"/api/couch", to: couch + "/"}
@@ -35,7 +32,7 @@ var couch = "http://localhost:5984";
 // ;
 
 function files(app) {
-    app.get('/index.html'
+    app.get('/'
             ,connect.static(__dirname+"/attachments")
            );
     app.get(/\/(css|js)\/.*/
@@ -44,36 +41,15 @@ function files(app) {
 
 }
 
-function user(app){
+function wim(app){
     app.get('/db/wim/:id/:attachment?'
-           ,function(req,res,next){
-                var to = couch + '/vdsdata%2ftracking/'+req.params.id;
-                if(req.params.attachment !== undefined){
-                    to += '/'+req.params.attachment;
-                }
-                console.log(to);
-                request(to).pipe(res);
-            });
+           ,rewriter);
+}
+function vds(app){
+    app.get('/db/vds/:id/:attachment?'
+           ,rewriter);
 }
 
-server.listen(80);
-console.log('Connect server listening on port 80 on service /banner');
-//server.listen(3000);
+server.listen(3001);
+console.log('Connect server listening on port 3001');
 console.log('Current gid: ' + process.getgid());
-try {
-    process.setgid(65533);
-    console.log('New gid: ' + process.getgid());
-}
-catch (err) {
-    console.log('Failed to set gid: ' + err);
-    throw(err);
-}
-console.log('Current uid: ' + process.getuid());
-try {
-    process.setuid(65534);
-    console.log('New uid: ' + process.getuid());
-}
-catch (err) {
-    console.log('Failed to set uid: ' + err);
-    throw(err);
-}
