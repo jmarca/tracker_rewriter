0df8652a851c91a6e8e616fb0cc6a9a90470de26
James E. Marca <jmarca@translab.its.uci.edu>
2012-02-07 17:13:53 -0800

    working on scroll.  not getting all the detectors need to %22 wrap start, end keys
    for now, ditching the infinite scroll idea.  just get them all in blocks of 100
    seems like it will work....if I can get it to work!
    
    modified:   attachments/css/layout.css
    modified:   attachments/js/dataview.js
    modified:   attachments/js/util.js

--- attachments/js/util.js
+++ attachments/js/util.js
@@ -1,136 +1,132 @@
-var util = function() {
-    function showLoader() {
-        $( '.loading' ).toggle();
-    }
+var util =
+    function() {
+        function showLoader() {
+            $( '.loading' ).toggle();
+        }
 
-    function hideLoader() {
-        $( '.loading' ).toggle();
-    }
+        function hideLoader() {
+            $( '.loading' ).toggle();
+        }
 
-    function loaderShowing() {
+        function loaderShowing() {
         var showing = false;
-        if( $( '.loading' ).css( 'visibility' ) !== "hidden" ) showing = true;
-        return showing;
-    }
-
-    var oldlast = 0;
-    var wimoldlast = 'wim.0';
-
-    function adddocs(container,docs){
-
-        container.selectAll('div')
-                 .data(docs)
-                 .enter()
-                 .append('div')
-                 .append('a')
-                 .attr('href','#')
-                 .on('click',function(d){
-                             getWIM(d.id);
-                             return false;
-                         })
-                 .text(function(d){ return d.id; });
-
-    }
-
-
-    function getMoreDocs(settings){
-        var defaults = {couch:''
-                       ,lookahead:400
-                       };
-        if(settings){
-            settings = _.extend(defaults,settings);
-        }else{
-            settings = defaults;
+            if( $( '.loading' ).css( 'visibility' ) !== "hidden" ) showing = true;
+            return showing;
         }
-        var url = settings.couch + '/db/detectors';
 
-        return function(){
+        var oldlast = '.';
+        var wimoldlast = 'wim.0';
 
-            var query = {limit:settings.lookahead
-                        ,startkey:oldlast
-                        ,endkey:1300000
-                        };
-            var option = jQuery('input:radio[name=dataset]:checked').val();
-            var lasthandler = function(err,data){
-                if(!err)
-                    oldlast = _.last(data);
-            }
-            var elemclass = '.vds';
+        var adddocs =
+            function(){
+                var alldocs = {};
+                return function(option,container,docs){
+                    if(alldocs[option] === undefined) {
+                        alldocs[option] = [];
+                    }
 
-            if (option === 'wim') {
-                query.startkey=wimoldlast;
-                query.endkey='wim.Z';
-                lasthandler =  function(err,data){
-                    if(!err)
-                        wimoldlast = _.last(data);
+                    alldocs[option] = _.flatten([alldocs[option],docs]);
+                    container.selectAll('div')
+                    .data(alldocs[option],function(d){
+                        return d.id;
+                    })
+                    .enter()
+                    .append('div')
+                    .append('a')
+                    .attr('href','#')
+                    .on('click',function(d){
+                        getWIM(d.id);
+                        return false;
+                    })
+                    .text(function(d){ return d.id; });
+
                 }
-                elemclass = '.wim';
+            }();
+
+
+        function getMoreDocs(settings){
+            var defaults = {couch:''
+                           ,lookahead:100
+                           };
+            if(settings){
+                settings = _.extend(defaults,settings);
+            }else{
+                settings = defaults;
             }
-            var success = function(wrapper,cb){
-                // display the data  in the big container.
-                return function(data){
-                    var rows = data.rows;
-                    if (rows.length > 0) {
-                        var last = _.last(rows);
-                        if (rows.length > 1) {
-                            rows.pop()
+            var url = settings.couch + '/db/detectors';
+
+            var getsome =
+                function(option){
+
+                    var query = {limit:settings.lookahead
+                                ,startkey:oldlast
+                                ,endkey:'1300000'
+                                };
+
+                    var lasthandler = function(err,newlast,next){
+                            if(!err)
+                                oldlast = newlast;
+                            if(next) next()
+
                         }
+                    var elemclass = '.vds';
 
-                        var container = d3.select('.detectors'+wrapper+' div.listing');
-                        adddocs(container,rows);
-
-                        cb(null,last.id)
-                        }else{
-                            cb('no data')
+                    if (option === 'wim') {
+                        query.startkey=wimoldlast;
+                        query.endkey='wim.Z';
+                        lasthandler =  function(err,newlast,next){
+                            if(!err)
+                                wimoldlast = newlast;
                         }
-                }
-            }(elemclass,lasthandler);
-            jQuery.ajax({'url': url
-                        ,'data': query
-                        ,'dataType': 'json'
-                           ,'success': success
-                           ,'cache': true
-                           });
+                        elemclass = '.wim';
+                    }
 
+                    var success =
+                        function(wrapper,cb){
+                            // display the data  in the big container.
+                            return function(data){
+                                var rows = data.rows;
+                                var next = null;
+                                if (rows.length > 0) {
+                                    var last = _.last(rows);
+                                    if (rows.length > 1 && rows.length==settings.lookahead) {
+                                        rows.pop()
+                                        next = fetch;
+                                    }
+
+                                    var container = d3.select('.detectors'+wrapper+' div.listing');
+                                    adddocs(option,container,rows);
+
+                                    cb(null,last.id,next)
+                                }else{
+                                    cb('no data')
+                                }
+                            }
+                        }(elemclass,lasthandler);
+                    function fetch(){
+                        query.startkey = option == 'wim' ? wimoldlast : oldlast;
+                        jQuery.ajax({'url': url
+                                    ,'data': query
+                                    ,'dataType': 'json'
+                                    ,'success': success
+                                    ,'cache': true
+                                    });
+
+                    };
+                    fetch();
+                };
+
+            return getsome;
         };
-    };
 
-    var doit = getMoreDocs();
 
-    function bindInfiniteScroll() {
-        var settings = {
-            couch: '',
-            lookahead: 400,
-            container: jQuery( '#detectors' )
-        };
+        var doit = getMoreDocs();
 
-        var scrollDown = function( e ) {
-            if ( loaderShowing() ) {
-                return;
-            }
 
-            var containerScrollTop = settings.container.scrollTop();
-            if ( ! containerScrollTop ) {
-                var ownerDoc = settings.container.get().ownerDocument;
-                if( ownerDoc ) {
-                    containerScrollTop = jQuery( ownerDoc.body ).scrollTop();
-                }
-            }
-            var distanceToBottom = jQuery( document ).height() - ( containerScrollTop + jQuery( window ).height() );
 
-            if ( distanceToBottom < settings.lookahead ) {
-                showLoader()
-                doit()
-            }
-
-        }
-        jQuery( window ).scroll( scrollDown );
-        scrollDown();
-    };
     return { showLoader: showLoader
            , hideLoader: hideLoader
            , loaderShowing: loaderShowing
-           , bindInfiniteScroll: bindInfiniteScroll
            , getMore: doit
            };
 
