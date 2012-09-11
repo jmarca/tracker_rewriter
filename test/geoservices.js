a9d8987b9a13f062bd17cb1d1fba36dc8645f06e
James E. Marca <jmarca@translab.its.uci.edu>
2012-09-11 16:53:22 -0700

    rock and roll added x,y,z version (zoom, col, row.json)
    modified:   lib/geocouch_rewriter.js
    modified:   lib/rewrite.js
    deleted:    lib/utils.js
    modified:   package.json
    modified:   test/geoservices.js

--- test/geoservices.js
+++ test/geoservices.js
@@ -17,8 +17,11 @@ var env = process.env;
 var testhost = env.TEST_HOST || '127.0.0.1'
 var testport = env.TEST_PORT || 3000
 
-
 var app,server
+
+var z = 10
+var c = 176
+var r = 409
 
 before(
     function(done){
@@ -65,6 +68,30 @@ describe('/ml detectors',function(){
                    return done()
            })
        })
+    it('should return a list of mainline detectors in a bbox'
+      ,function(done){
+           superagent
+           .get('http://'+ testhost +':'+testport+['/ml',z,c,r+'.json'].join('/'))
+           .set('accept','application/json')
+           .set('followRedirect',true)
+           .end(function(err,res){
+               if(err) return done(err)
+               res.ok.should.be.true
+               should.exist(res.body)
+               var c = res.body
+               c.should.have.property('features')
+               c.features.length.should.equal(550)
+               _.each(c.features
+                     ,function(row){
+                          row.should.have.property('type')
+                          row.should.have.property('geometry')
+                          row.should.have.property('properties')
+                          row.properties.should.have.property('id')
+                          row.properties.id.should.match(/^\d{6,7}$/)
+                      })
+                   return done()
+           })
+       })
 })
 
 describe('/hv detectors',function(){
@@ -86,6 +113,30 @@ describe('/hv detectors',function(){
                var c = res.body
                c.should.have.property('features')
                c.features.length.should.be.below(11)
+               _.each(c.features
+                     ,function(row){
+                          row.should.have.property('type')
+                          row.should.have.property('geometry')
+                          row.should.have.property('properties')
+                          row.properties.should.have.property('id')
+                          row.properties.id.should.match(/^\d{6,7}$/)
+                      })
+                   return done()
+           })
+       })
+    it('should return a list of hov detectors in a bbox'
+      ,function(done){
+           superagent
+           .get('http://'+ testhost +':'+testport+['/hv',z,c,r+'.json'].join('/'))
+           .set('accept','application/json')
+           .set('followRedirect',true)
+           .end(function(err,res){
+               if(err) return done(err)
+               res.ok.should.be.true
+               should.exist(res.body)
+               var c = res.body
+               c.should.have.property('features')
+               c.features.length.should.equal(438)
                _.each(c.features
                      ,function(row){
                           row.should.have.property('type')
@@ -129,6 +180,30 @@ describe('/cd detectors',function(){
                    return done()
            })
        })
+    it('should return a list of collector/distributor detectors in a bbox'
+      ,function(done){
+           superagent
+           .get('http://'+ testhost +':'+testport+['/cd',z,c,r+'.json'].join('/'))
+           .set('accept','application/json')
+           .set('followRedirect',true)
+           .end(function(err,res){
+               if(err) return done(err)
+               res.ok.should.be.true
+               should.exist(res.body)
+               var c = res.body
+               c.should.have.property('features')
+               c.features.length.should.equal(11)
+               _.each(c.features
+                     ,function(row){
+                          row.should.have.property('type')
+                          row.should.have.property('geometry')
+                          row.should.have.property('properties')
+                          row.properties.should.have.property('id')
+                          row.properties.id.should.match(/^\d{6,7}$/)
+                      })
+                   return done()
+           })
+       })
 })
 
 describe('/ff detectors',function(){
@@ -150,6 +225,30 @@ describe('/ff detectors',function(){
                var c = res.body
                c.should.have.property('features')
                c.features.length.should.be.below(11)
+               _.each(c.features
+                     ,function(row){
+                          row.should.have.property('type')
+                          row.should.have.property('geometry')
+                          row.should.have.property('properties')
+                          row.properties.should.have.property('id')
+                          row.properties.id.should.match(/^\d{6,7}$/)
+                      })
+                   return done()
+           })
+       })
+    it('should return a list of freeway to freeway detectors in a bbox'
+      ,function(done){
+           superagent
+           .get('http://'+ testhost +':'+testport+['/ff',z,c,r+'.json'].join('/'))
+           .set('accept','application/json')
+           .set('followRedirect',true)
+           .end(function(err,res){
+               if(err) return done(err)
+               res.ok.should.be.true
+               should.exist(res.body)
+               var c = res.body
+               c.should.have.property('features')
+               c.features.length.should.equal(107)
                _.each(c.features
                      ,function(row){
                           row.should.have.property('type')
@@ -193,6 +292,30 @@ describe('/fr detectors',function(){
                    return done()
            })
        })
+    it('should return a list of off ramp detectors in a bbox'
+      ,function(done){
+           superagent
+           .get('http://'+ testhost +':'+testport+['/fr',z,c,r+'.json'].join('/'))
+           .set('accept','application/json')
+           .set('followRedirect',true)
+           .end(function(err,res){
+               if(err) return done(err)
+               res.ok.should.be.true
+               should.exist(res.body)
+               var c = res.body
+               c.should.have.property('features')
+               c.features.length.should.equal(229)
+               _.each(c.features
+                     ,function(row){
+                          row.should.have.property('type')
+                          row.should.have.property('geometry')
+                          row.should.have.property('properties')
+                          row.properties.should.have.property('id')
+                          row.properties.id.should.match(/^\d{6,7}$/)
+                      })
+                   return done()
+           })
+       })
 })
 
 describe('/or detectors',function(){
@@ -214,6 +337,30 @@ describe('/or detectors',function(){
                var c = res.body
                c.should.have.property('features')
                c.features.length.should.be.below(11)
+               _.each(c.features
+                     ,function(row){
+                          row.should.have.property('type')
+                          row.should.have.property('geometry')
+                          row.should.have.property('properties')
+                          row.properties.should.have.property('id')
+                          row.properties.id.should.match(/^\d{6,7}$/)
+                      })
+                   return done()
+           })
+       })
+    it('should return a list of onramp detectors in a bbox'
+      ,function(done){
+           superagent
+           .get('http://'+ testhost +':'+testport+['/or',z,c,r+'.json'].join('/'))
+           .set('accept','application/json')
+           .set('followRedirect',true)
+           .end(function(err,res){
+               if(err) return done(err)
+               res.ok.should.be.true
+               should.exist(res.body)
+               var c = res.body
+               c.should.have.property('features')
+               c.features.length.should.equal(289)
                _.each(c.features
                      ,function(row){
                           row.should.have.property('type')
