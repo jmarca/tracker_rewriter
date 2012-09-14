9e4df7118cb28921ab8cf3799f5fc9ae36a382b1
James E. Marca <jmarca@translab.its.uci.edu>
2012-09-14 11:58:43 -0700

    add static webfiles server route.  also make it work and test it
    had to move all these files so that the path '/tracker' can find them
     renamed:    attachments/css/layout.css -> attachments/tracker/css/layout.css
     renamed:    attachments/css/oocss -> attachments/tracker/css/oocss
     renamed:    attachments/css/ui.css -> attachments/tracker/css/ui.css
     renamed:    attachments/index.html -> attachments/tracker/index.html
     renamed:    attachments/js/async.min.js -> attachments/tracker/js/async.min.js
     renamed:    attachments/js/dataview.js -> attachments/tracker/js/dataview.js
     renamed:    attachments/js/detector_view.js -> attachments/tracker/js/detector_view.js
     renamed:    attachments/js/handlebars.js -> attachments/tracker/js/handlebars.js
     renamed:    attachments/js/jquery-1.6.3.min.js -> attachments/tracker/js/jquery-1.6.3.min.js
     renamed:    attachments/js/jquery.js -> attachments/tracker/js/jquery.js
     renamed:    attachments/js/map.js -> attachments/tracker/js/map.js
     renamed:    attachments/js/polymaps.min.js -> attachments/tracker/js/polymaps.min.js
     renamed:    attachments/js/ui.js -> attachments/tracker/js/ui.js
     renamed:    attachments/js/underscore.js -> attachments/tracker/js/underscore.js
     renamed:    attachments/js/util.js -> attachments/tracker/js/util.js
     renamed:    attachments/js/vdsview.js -> attachments/tracker/js/vdsview.js
     renamed:    attachments/js/wimview.js -> attachments/tracker/js/wimview.js
    modified:   index.js
    new file:   lib/tracker_webfiles.js
    modified:   test/db_path.js
    new file:   test/static_webfiles.js

---
+++ test/static_webfiles.js
@@ -0,0 +1,84 @@
+//test the webfiles server
+
+/* global require console process it describe after before */
+
+var should = require('should')
+
+var superagent = require('superagent')
+var http = require('http')
+
+var tracker = require('../.').tracker
+var express = require('express')
+
+var env = process.env;
+var testhost = env.TEST_HOST || '127.0.0.1'
+var testport = env.TEST_PORT || 3000
+testport += 2
+
+var app,server
+
+before(
+    function(done){
+        app = express()
+              .use(express.logger())
+
+        tracker(app)
+        server=http
+               .createServer(app)
+               .listen(testport,done)
+
+    })
+after(function(done){
+    server.close(done)
+})
+
+describe('/tracker path',function(){
+    it('should get index.html from /tracker'
+      ,function(done){
+           superagent
+           .get('http://'+ testhost +':'+testport+'/tracker')
+           .end(function(err,res){
+               if(err) return done(err)
+               res.ok.should.be.true
+               res.text.should.match(/Detector imputation details/)
+               return done()
+           })
+
+       })
+    it('should get index.html from /tracker/'
+      ,function(done){
+           superagent
+           .get('http://'+ testhost +':'+testport+'/tracker/')
+           .end(function(err,res){
+               if(err) return done(err)
+               res.ok.should.be.true
+               res.text.should.match(/Detector imputation details/)
+               return done()
+           })
+
+       })
+    it('should not get index.html from /trackers'
+      ,function(done){
+           superagent
+           .get('http://'+ testhost +':'+testport+'/trackers')
+           .end(function(err,res){
+               if(err) return done(err)
+               res.ok.should.be.false
+               return done()
+           })
+
+       })
+    it('should get index.html from /tracker/index.html'
+      ,function(done){
+           superagent
+           .get('http://'+ testhost +':'+testport+'/tracker/index.html')
+           .end(function(err,res){
+               if(err) return done(err)
+               res.ok.should.be.true
+               res.text.should.match(/Detector imputation details/)
+               return done()
+           })
+
+       })
+
+})
